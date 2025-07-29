55dba39d0206bfb4031c31897f7bc440171254c3
randilwelikala <randilgimantha646@gmail.com>
2025-07-29 10:45:44 +0530

    create cardless side navbar

---
+++ src/components/cardlessSideNavbar/cardlessSideNavbar.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './cardSideNavbar.css';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+export default function CardlessSideNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navbarRef = useRef(null);
+  const [searchParams] = useSearchParams();
+  const accountNumber = searchParams.get('account');
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      navbarRef.current &&
+      !navbarRef.current.contains(event.target) &&
+      !event.target.classList.contains('fixed-toggle-btn')
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  return (
+    <>
+      <button className="fixed-toggle-btn" onClick={toggleNavbar}>
+        ☰
+      </button>
+
+      <div ref={navbarRef} className={`right-navbar ${isOpen ? 'open' : ''}`}>
+        <div className="navbar-title">Cardless Transactions</div>
+        <button onClick={() => window.location.href = `/askReceiptforCardlessWithdraw`}>Withdraw Money</button>
+        <button onClick={() => window.location.href = `/askCardless`}>Deposit Money</button>
+        <button onClick={() => window.location.href = `/downloadHistoryOTP`}>Download History</button>
+     
+      </div>
+    </>
+  );
+}
